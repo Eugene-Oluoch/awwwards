45ae324ad56ef0c1fdc77e99644683c21ac36f7f
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-04-09 16:46:59 +0300

    Configured app to use cloudinary

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awards.app.AwardsConfig',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+cloudinary.config( 
+    cloud_name = "dnesn8pce", 
+    api_key = "588882449298784", 
+    api_secret = "tDd9VmBqnNWuc8h1TK4eKB7O41g" 
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
