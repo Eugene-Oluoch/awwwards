309089f2a2317ee814b0887bc25fe869a0d3fd17
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-04-10 20:59:53 +0300

    deployment after style didn't load

--- app/settings.py
+++ app/settings.py
@@ -148,10 +148,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-]
+)
 cloudinary.config( 
     cloud_name = "dnesn8pce", 
     api_key = "588882449298784", 
