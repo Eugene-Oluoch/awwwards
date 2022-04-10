8dfd1bb2669d4fa1bf0c0be0dfbbc90f4ba7078d
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-04-10 21:06:36 +0300

    Configured some staticsetting

--- app/settings.py
+++ app/settings.py
@@ -150,6 +150,17 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static/')
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 # Extra places for collectstatic to find static files.
 
